7bbcf1d40e8355d0bbdb737ba6e203a676a77224
Hisham Khattab <42070333+hishamkhattab@users.noreply.github.com>
2021-12-30 21:48:28 +0200

    adding dice component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Dice from "./Dice"
+
+
+export default function App() {
+    return (
+        <div className='dice'>
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+        </div>
+    )
+}
---
+++ src/Dice.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dice() {
+    return (
+        <div className="die-container">
+            <h3>1</h3>
+        </div>
+    )
+}
