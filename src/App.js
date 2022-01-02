b03544aad357ff019a8b65a2c6138e45dcb03e8b
Hisham Khattab <42070333+hishamkhattab@users.noreply.github.com>
2022-01-02 12:35:00 +0200

    adding the time board

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import Dice from "./components/Dice"
 import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
 import WinBoard from "./components/WinBoard"
-
+import {useWindowSize} from 'react-use';
 
 export default function App() {
 
-    const [dice, setDice] = useState(generateDice())
-    const [tenzies, setTenzies] = useState(false)
+    //get the width and the height of the screen
+    const { width, height } = useWindowSize();
 
+    /***********************
+     * set states **********
+     ***********************/
+    
+    //control the dice
+    const [dice, setDice] = useState(generateDice())
+    //control when to win
+    const [tenzies, setTenzies] = useState(false)
+    //control number of steps to win
     const [steps, setSteps] = useState(0)
-    const [time, setTime] = useState(0)
+    //constrol the time the game started
+    const [start, setStartTime] = useState('');
+    //constrol when the game started
+    const [hasStarted, setHasStarted] = useState(false);
+    //constrol the time the game ended
+    const [end, setEndTime] = useState('');
+    //control the best record board
+    const [showBoard, setShowBoard] = useState(false)
+
 
 
     /**
@@ -22,18 +39,24 @@ export default function App() {
         const firstValue = dice[0].value;
         const allSame = dice.every(die => die.value === firstValue);
         if (allHeld && allSame) {
-            setTenzies(true)
+            let date = new Date();
+
+            setEndTime(date.getTime());
+            setHasStarted(false);
+            setTenzies(true);
         }
-    },[dice])
+    }, [dice])
 
 
-    
+    /******************
+     * functions ******
+     *****************/
+
     /**
      * function to imitate the dice throughing 
      * @returns an array of length 10 with random values from (1 - 6)
      * it will only generated in the start of the game
      */
-
     function generateDice() {
         const diceArray = []
         for (let i = 0; i < 10; i++) {
@@ -46,18 +69,20 @@ export default function App() {
     // return a die with unique id and random number from (1-6)
     function diceObject() {
         return {
-            id:nanoid(),
+            id: nanoid(),
             value: Math.ceil(Math.random() * 6),
-            isHeld:false
+            isHeld: false
         }
     }
+
+
     /**
      * when clicking on a die it hold it
      * @param {*} id the id of the clicked die
      */
     function holdDice(id) {
-        setDice(prevState => prevState.map( die => {
-            return die.id === id ? {...die, isHeld: !die.isHeld} : die 
+        setDice(prevState => prevState.map(die => {
+            return die.id === id ? { ...die, isHeld: !die.isHeld } : die
         }))
     }
 
@@ -68,16 +93,22 @@ export default function App() {
      * generate a new set of dice for the ones that didn't get selected
      */
     function rollDice() {
+        let date = new Date();
+        showBoard(false)
+        if (hasStarted === false) {
+            setHasStarted(true);
+            setStartTime(date.getTime())
+        }
+
         if (!tenzies) {
             setDice(prevState => prevState.map(die => {
                 return die.isHeld ? die : diceObject()
             }))
-            setSteps(prevTime => prevTime +1)
+            setSteps(prevTime => prevTime + 1)
         } else {
             setTenzies(false);
             setDice(generateDice());
             setSteps(0)
-            setTime(0)
         }
     }
 
@@ -87,34 +118,40 @@ export default function App() {
      * passing a random number at each instance 
      */
     const diceElement = dice.map(die => {
-        return <Dice key={die.id} {...die} clickHandle={holdDice}/>
+        return <Dice key={die.id} {...die} clickHandle={holdDice} />
     })
 
 
 
-    /** the timer */
-    function tick() {
-        setTime( timer => timer + 1)
+    /**
+     * looping over local storage to render the 3 best times
+     */
+    const bestTimes = localStorage.getItem("Times") ?
+        JSON.parse(localStorage.getItem("Times")) : [];
+    
+    let bestElements = [];
+    bestElements = bestTimes.slice(0,3).sort((a, b) => a - b).map((time, idx) => {
+        return (
+            <span key={idx}>{time}</span>
+        )
+    })
+
+    function showRecord() {
+            setShowBoard( prev => !prev)
     }
-
-    useEffect(() => {
-        const interval = setInterval(tick, 1000)
-        return () => {
-            clearInterval(interval)
-        }
-    },[])
-
 
     return (
         <main>
-            {tenzies && <Confetti />}
+            {tenzies && <Confetti width={width} height={height}/>}
             <h1 className="title">Tenzies</h1>
             <p className="instruction">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className='dice'>
                 {diceElement}
             </div>
             <button onClick={rollDice}>{tenzies ? "NEW GAME" : "ROLL"}</button>
-            {tenzies && <WinBoard steps={steps} />}
+            {tenzies && <WinBoard steps={steps} startTime={start} endTime={end} />}
+            <button onClick={showRecord}>SHOW RECORD</button>
+            {showBoard && <div className="record"> {bestElements} </div>}
             </main>
             )
-        }
+}
--- src/components/WinBoard.js
+++ src/components/WinBoard.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
 function WinBoard(props) {
+    const diff = (props.endTime - props.startTime) / 1000;
+    const timeArray = localStorage.getItem('Times') ? JSON.parse(window.localStorage.getItem('Times')) : [];
+    timeArray.push(diff.toFixed(2));
+
+    
+    React.useEffect(() => {
+        window.localStorage.setItem("Times",JSON.stringify(timeArray))
+    })
+
     return (
         <div className='board'>
-            <p>Time: {props.time}</p>
-            <p>Steps: {props.steps}</p>
+            <span>Time: {diff.toFixed(2)} Sec</span>
+            <span>Steps: {props.steps}</span>
+            <span>{timeArray.sort( (a,b) => a-b )} | </span>
         </div>
     )
 }
